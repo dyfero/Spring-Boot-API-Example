cfc3e06dad5a3abae21aea8f10aba5d28ff2256b
Tomasz Smolarek <ts.smolarek@gmail.com>
2026-04-14 19:41:28 +0200

    Initial commit

---
+++ src/main/java/com/github/dyfero/springbootapiexample/SpringbootapiexampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.dyfero.springbootapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapiexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootapiexampleApplication.class, args);
+    }
+
+}
